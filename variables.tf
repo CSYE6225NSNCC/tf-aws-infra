3060a882c6a651cc1b82025b87244e2621da4992
amrutamuzumdar <muzumdar.a@northeastern.edu>
2024-10-10 02:38:27 -0400

    Remove defaults and rename tags

--- variables.tf
+++ variables.tf
@@ -1,32 +1,28 @@
 variable "aws_region" {
   description = "The AWS region to deploy the resources"
-  default     = "us-east-1" # Change to your desired region
+
 }
 
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
-  default     = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidrs" {
   description = "CIDR blocks for public subnets"
   type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  
 }
 
 variable "private_subnet_cidrs" {
   description = "CIDR blocks for private subnets"
   type        = list(string)
-  default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 }
 
 variable "availability_zones" {
   description = "Availability zones for the subnets"
   type        = list(string)
-  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
 
 variable "aws_profile" {
   description = "AWS profile to use"
-  default     = "default"
 }
