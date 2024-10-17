47d1667a02e22d061bba599f0505f5f06ed2a1f5
amrutamuzumdar <muzumdar.a@northeastern.edu>
2024-10-17 06:10:13 -0400

    Add EC2 and security group

--- variables.tf
+++ variables.tf
@@ -27,3 +27,24 @@ variable "aws_profile" {
   description = "AWS profile to use"
   type        = string
 }
+
+##Variables for ec2 and security group
+
+variable "custom_ami_id" {
+  description = "The ID of the custom AMI to use."
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The type of EC2 instance."
+  default     = "t2.micro" // Adjust according to your needs
+}
+
+variable "application_port" {
+  description = "The port on which the application runs."
+  default     = 3000 // Replace with your application's port
+}
+
+variable "key_name" {
+  default = "ubuntu"
+}
